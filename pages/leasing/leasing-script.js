beaec9dc284eba14dc37605d426cfa4fddd3b1a1
theibd56 <igorbezhinarov@yandex.ru>
2026-02-02 17:13:39 +0300

    табуляция

--- pages/leasing/leasing-script.js
+++ pages/leasing/leasing-script.js
@@ -3,9 +3,9 @@ import {Navigation, Pagination, Controller, EffectFade, Autoplay, Thumbs} from '
 
 Swiper.use([Navigation, Pagination, Controller, EffectFade, Autoplay, Thumbs]);
 
-//FAQ
-    const items = document.querySelectorAll('.leasing-faq__item');
+const items = document.querySelectorAll('.leasing-faq__item');
 
+if (items.length) {
     items.forEach(item => {
         item.addEventListener('click', () => {
             const isActive = item.classList.contains('is-active');
@@ -17,112 +17,110 @@ Swiper.use([Navigation, Pagination, Controller, EffectFade, Autoplay, Thumbs]);
             }
         });
     });
+}
 
-//slider
-  const leasingProductSlider = new Swiper('.leasing__products_slider .swiper', {
-      slidesPerView: 4,
-      spaceBetween: 8,
-      loop: true,
-      navigation: {
-          nextEl: '.leasing__products_next',
-          prevEl: '.leasing__products_prev',
-      },
-      breakpoints: {
-          0: {
-              slidesPerView: 1.5,
-              spaceBetween: 8,
-          },
-          480: {
-              slidesPerView: 2.3,
-              spaceBetween: 8,
-          },
-          600: {
-              slidesPerView: 2.7,
-              spaceBetween: 8,
-          },
-          768: {
-              slidesPerView: 3,
-              spaceBetween: 8,
-          },
-          900: {
-              slidesPerView: 3,
-              spaceBetween: 16,
-          },
-          1200: {
-              slidesPerView: 4,
-              spaceBetween: 16,
-          },  
-          1550: {
-              slidesPerView: 3,
-              spaceBetween: 16,
-          }, 
-          1860: {
-              slidesPerView: 4,
-              spaceBetween: 16,
-          }, 
-      }
-  });
+const leasingProductSlider = new Swiper('.leasing__products_slider .swiper', {
+    slidesPerView: 4,
+    spaceBetween: 8,
+    loop: true,
+    navigation: {
+        nextEl: '.leasing__products_next',
+        prevEl: '.leasing__products_prev',
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1.5,
+            spaceBetween: 8,
+        },
+        480: {
+            slidesPerView: 2.3,
+            spaceBetween: 8,
+        },
+        600: {
+            slidesPerView: 2.7,
+            spaceBetween: 8,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 8,
+        },
+        900: {
+            slidesPerView: 3,
+            spaceBetween: 16,
+        },
+        1200: {
+            slidesPerView: 4,
+            spaceBetween: 16,
+        },
+        1550: {
+            slidesPerView: 3,
+            spaceBetween: 16,
+        },
+        1860: {
+            slidesPerView: 4,
+            spaceBetween: 16,
+        },
+    }
+});
 
-//partners slider
 const partnersSwiper = new Swiper(".leasing__partners_slider", {
     slidesPerView: 5,
     spaceBetween: 8,
     loop: true,
     breakpoints: {
-      0: {
-        enabled: true,
-        slidesPerView: 1,
-      },
-      200: {
-        slidesPerView: 1,
-      },
-      360: {
-        slidesPerView: 1.5,
-      },
-      480: {
-        slidesPerView: 2.2,
-      },
-      600: {
-        slidesPerView: 2.9,
-      },
-      768: {
-        slidesPerView: 2.9,
-      },
-      900: {
-        slidesPerView: 3.5,
-        spaceBetween: 16,
-      },
-      1024: {
-        enabled: false,
-        spaceBetween: 16, 
-      },
+        0: {
+            enabled: true,
+            slidesPerView: 1,
+        },
+        200: {
+            slidesPerView: 1,
+        },
+        360: {
+            slidesPerView: 1.5,
+        },
+        480: {
+            slidesPerView: 2.2,
+        },
+        600: {
+            slidesPerView: 2.9,
+        },
+        768: {
+            slidesPerView: 2.9,
+        },
+        900: {
+            slidesPerView: 3.5,
+            spaceBetween: 16,
+        },
+        1024: {
+            enabled: false,
+            spaceBetween: 16,
+        },
     },
 });
-window.addEventListener("resize", initPartnersSwiper);
-window.addEventListener("load", initPartnersSwiper);
-
 
 //form
 const form = document.querySelector('.leasing__request-form');
 const inputs = form.querySelectorAll('.input-field');
 
-form.addEventListener('submit', (e) => {
-  let hasError = false;
+if (form && inputs) {
+    form.addEventListener('submit', (e) => {
+        let hasError = false;
 
-  inputs.forEach(input => {
-    const errorEl = input.nextElementSibling;
+        inputs.forEach(input => {
+            const errorEl = input.nextElementSibling;
 
-    if (input.value.trim() === '') {
-      errorEl.textContent = 'Заполните это поле';
-      input.classList.add('error');
-      hasError = true;
-    } else {
-      errorEl.textContent = '';
-      input.classList.remove('error');
-    }
-  });
+            if (input.value.trim() === '') {
+                errorEl.textContent = 'Заполните это поле';
+                input.classList.add('error');
+                hasError = true;
+            } else {
+                errorEl.textContent = '';
+                input.classList.remove('error');
+            }
+        });
 
-  if (hasError) {
-    e.preventDefault();
-  }
-});
+        if (hasError) {
+            e.preventDefault();
+        }
+    });
+}
