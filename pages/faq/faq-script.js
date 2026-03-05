799ff007d5760b00acfeebd5df6ef7f12e607296
nikitadmtr <nikitabezhinarov@mail.ru>
2026-03-05 18:50:36 +0700

    малые правки

--- pages/faq/faq-script.js
+++ pages/faq/faq-script.js
@@ -3,32 +3,6 @@ import {Navigation, Pagination, Controller, EffectFade, Autoplay, Thumbs} from '
 
 Swiper.use([Navigation, Pagination, Controller, EffectFade, Autoplay, Thumbs]);
 
-//form
-const form = document.querySelector('.faq__feedback-form');
-const inputs = form.querySelectorAll('.input-field');
-
-if (form && inputs) {
-    form.addEventListener('submit', (e) => {
-        let hasError = false;
-
-        inputs.forEach(input => {
-            const errorEl = input.nextElementSibling;
-
-            if (input.value.trim() === '') {
-                errorEl.textContent = 'Заполните это поле';
-                input.classList.add('error');
-                hasError = true;
-            } else {
-                errorEl.textContent = '';
-                input.classList.remove('error');
-            }
-        });
-
-        if (hasError) {
-            e.preventDefault();
-        }
-    });
-}
 
 const navLinks = document.querySelectorAll('.faq__nav-link');
 
